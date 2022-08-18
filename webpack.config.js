ba264e1ed076c0c12cfd60d837e77f0aa7909a49
theshy456 <lbs521lly@gmail.com>
2022-08-18 22:10:55 +0800

    'update'

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
     mode:'development',//给自己看的。production发布，
     entry: './src/index.js',
     output: {
 
-        filename: '[name].[contenthash].js',//文件名和内容一一对应，http缓存更新
+        filename: 'index.[contenthash].js',//文件名和内容一一对应，http缓存更新
+    },
+    plugins: [new HtmlWebpackPlugin(
+        {
+            title: '彭于晏',
+            template: 'src/assets/index.html'//基于assets的index创建dist中的index
+        }
+    )],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,//$正则表达式
+                use: ["style-loader", "css-loader"],//css将文件读到js里面，style将独到的css放到header种
+            },
+        ],
     },
 };
